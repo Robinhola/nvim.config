db950b3f153a935415f18527446af53aed85a737
Robin Elrharbi-Fleury <robin.elrharbifleury@gmail.com>
2023-11-08 21:55:49 +0000

    Add some ctrl based remaps

--- lua/robinef/remap.lua
+++ lua/robinef/remap.lua
@@ -13,5 +13,11 @@ vim.keymap.set('n', '<leader>w', '<c-w>w')
 vim.keymap.set('n', '<leader>W', '<c-w>W')
 vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
 vim.keymap.set('n', '<leader>Q', '<cmd>quit!<CR>')
+-- Using control
+vim.keymap.set('n', '<c-q>', '<cmd>quit<CR>')
+vim.keymap.set('n', '<c-s>', '<cmd>split<CR>')
+vim.keymap.set('n', '<c-d>', '<cmd>vsplit<CR>')
+vim.keymap.set('n', '<c-x>', '<cmd>write<CR>')
+vim.keymap.set('n', '<c-c>', '<escape>')
 -- Copy without erasing
 vim.keymap.set("x", "<leader>p", "\"_dP")
